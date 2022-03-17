a20d6cd0faff7d46727b101b2857851181996f59
alexuma <m5a97b@gmail.com>
2022-03-17 17:24:16 -0300

    btn regresar al top

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ import hamburgerMenu from "./hamburger.js";
 import { digitalClock, alarm } from "./reloj.js";
 import { moveBall, shortcuts } from "./teclado.js";
 import countdown from "./cuenta_regresiva.js";
+import scrollTopButtom from "./boton_scroll.js";
 const d = document;
 
 d.addEventListener("DOMContentLoaded", (e) => {
@@ -17,6 +18,7 @@ d.addEventListener("DOMContentLoaded", (e) => {
     "oct 24,2022",
     "Feliz Cumple al programador de esta pagina"
   );
+  scrollTopButtom(".scroll-top-btn");
 });
 d.addEventListener("keypress", (e) => {
   shortcuts(e);
